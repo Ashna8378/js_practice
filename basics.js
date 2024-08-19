d9bd878eaecd2b1604e2fc89b9d8e2c575de6463
Ashna123 <ashna22@navgurukul.org>
2024-08-19 16:56:27 +0530

    initial_commit

---
+++ basics.js
@@ -0,0 +1,32 @@
+// Javascript = Javascript is a scripting language that is used to add interactivity
+
+// console.log("hello world")
+
+// Variable = variable is just like a container to store data/value
+
+
+// Javascript variables can be declared in 4 ways 
+
+// automatically           // doubt
+// using var
+// using let
+// using const 
+
+// When to use var,let or const?
+
+// const - if the value should not be changed
+// const - if the type should not be changed (Array and objects)
+// let - if uh can't use const 
+// var - if support old browser
+
+
+// Rules for consructing names for variables(unique identifiers) 
+
+// Name can contain letters, digits, underscores, and dollar signs
+// Name must begin with a letter
+// Names can also begin with $ and _
+// Names are case - sensitive
+// reserved words cannot be used as variable's name 
+
+
+
