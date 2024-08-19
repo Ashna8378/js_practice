9cbff1d44177b383161c85043b109054664aa187
Ashna123 <ashna22@navgurukul.org>
2024-08-19 15:59:37 +0530

    initial_commit

---
+++ script.js
@@ -0,0 +1,156 @@
+// console.log("ashna")
+
+// Nullish coalesing Operator - "??"
+
+// let a =5 
+// console.log(a?? "Hello");
+
+// let b = undefined
+// console.log(b ?? "Hello");
+
+// let firstName;
+// let lastName;
+// let nickName = "SuperCoder"
+// console.log(firstName ?? lastName ?? "Anonymous");
+// console.log(firstName ?? lastName ?? nickName ?? "Anonymous");
+
+
+// let height = 0
+// console.log(height ?? 100)
+// console.log(height || 100)
+
+
+// let height = 10
+// let width;
+
+// let area = (height ?? 100) * (width ?? 50)
+// console.log(area)
+
+
+
+// let height;
+// let width = 10
+
+// let area = (height ?? 100) * (50 ?? width)
+// console.log(area)
+// console.log(100 || 40)
+
+// Truthy and falsy values
+
+// falsy values - false , 0 , "" , null , undefined , NaN
+
+// console.log(Boolean(false))
+// console.log(Boolean(0))
+// console.log(Boolean(""))
+// console.log(Boolean(null))
+// console.log(Boolean(undefined))
+// console.log(Boolean(NaN))
+
+
+// let height = 0
+// console.log(height ?? 100)
+// console.log(height || false || NaN)
+
+// let a = ""
+// let b = ""
+// console.log(a ?? 5)
+// console.log(b || 10)
+
+// falsy values - false , 0 , "" , null , undefined , NaN
+
+
+// let isOpen = false
+
+// if (isOpen){
+//     console.log("The door is open")
+// }
+// else{
+//     console.log("we hav no apples")
+// }
+
+// let apples = 0;
+// if(apples) {
+//     console.log("we have apples")
+// }
+// else{
+//     console.log("we have no apples")
+// }
+
+// let input = ""
+// let message = input || "hello!";
+// console.log(message)
+
+// let fname;
+// if(fname){
+//     console.log("Hi"+ fname)
+
+// }
+// else{
+//     console.log("hi guest")
+// }
+
+// String comparison
+
+// console.log("Z">"A")
+// console.log("A" >"B")             //false
+// console.log("Priyanka" > "Punam"); //false
+
+// console.log("Priya">"Priyanka")   //false
+// console.log("Mee" > "Me")         //true
+
+// console.log("a" > "A")
+
+// console.log("2" > 1) // true   2 will convert into 2
+
+// console.log("01" == 1) // true
+
+// console.log(true > 0)  
+// console.log(true == 1)  // true
+
+// console.log("A" > 0)   // false
+// console.log("A" == 65) //false
+
+
+// console.log(typeof("0"))
+// console.log(typeof(0))
+// console.log(typeof(false))
+
+// console.log(null === undefined)  //false
+
+// console.log(null == undefined)  //true
+
+// console.log(undefined == 0)  // false
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+ 
+// if we compare undefined with any value other then null then it will give false
+
+// console.log(3>2)
+// console.log("orange" > "apple")
+// console.log("apple" > "Apple")
+// console.log("apple" > "app")
+// console.log("10" > "2")
+// console.log("10" > 2)
+// console.log("20" > "22")
+// console.log("20" >"2")
+// console.log("20" > 2)
+// console.log(undefined == null)
+// console.log(undefined === null)
+
+// Math.floor----------------------Rounds down
+
+// console.log(Math.floor(3.1))
+// console.log(Math.floor(3.9))
+// console.log(Math.floor(-3.1))
+// console.log(Math.floor(-3.9))
+
+// Math.ceil--------------------------------Rounds up
+
+// console.log(Math.ceil(3.1))
+// console.log(Math.ceil(3.9))
+// console.log(Math.ceil(-3.1))
+// console.log(Math.ceil(-3.9))
+
+
+
