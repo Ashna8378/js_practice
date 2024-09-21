dc31088d8b6ceb2ca26c643e381935541aea2ff8
Ashna123 <ashna22@navgurukul.org>
2024-09-21 09:15:30 +0530

    initial commit

--- basics.js
+++ basics.js
@@ -28,5 +28,126 @@
 // Names are case - sensitive
 // reserved words cannot be used as variable's name 
 
+// Uses 
+
+// Var - we can redeclare and update the variable
+// let - we can update the variable but not redeclare
+// const - we can't redeclare and update the variable 
+
+// Var - we can redeclare and update the variable-----------------------------------------------------------------------
+
+// var a = 10
+// console.log(a)
+
+
+// var a = 20  // Redeclaration is allowed
+// console.log(a)   // 20
+
+// a = 30 // updating the variable
+// console.log(a)
+
+// let - we can update the variable but not redeclare-------------------------------------------------------------------
+
+// let b = 10
+// console.log(b)
+
+// let b = 20  // error redeclaration is not allowed
+
+// b = 20     // updating the value
+// console.log(b)
+
+
+
+// const - we can't redeclare and update the variable----------------------------------------------------------
+
+
+// const c = 10
+// console.log(c)
+
+// // const c = 20 // error : redeclaration is not allowed
+
+// c = 20  // error : updating is not allowed
+
+// const c ; //error : must be initialize at the time of declaration
+
+
+// Data Types
+
+// 1. Primitive Data Types - String, Number, Boolean, Null, Undefined, Symbol
+// 2. Non - Primitive data types - Object
+
+// String
+
+// let word = "Hello world"
+
+// let word1 = 'hello'
+
+// let word2 = `hello`
+
+// Number 
+
+// let number = 10
+// let number1 = 10.5
+// let number2 = -6
+
+// Boolean
+
+// let value = true
+// let value1 = false
+
+// Null
+
+// let empty = null
+// console.log(empty)   // null
+
+// Undefined
+
+// let blank 
+// console.log(blank)
+
+
+
+// Non-Primitive  data types
+
+// Arrays - It is used to store multiple values in a single variable
+// index start with 0
+
+// let items = ["apple", 100, true, null]
+
+// console.log(items[2])
+
+// Object - It is used to store multiple values in a single variable in key-value pair
+
+// let items = {
+//     "fruitName" : "apple",
+//     "price" : 200,
+//     "Isavailable" : true
+// }
+
+// let price = 300
+
+// dot notation
+// square bracket notation
+
+// console.log(items.fruitName)
+// console.log(items.price)
+
+// console.log(items["fruitName"])
+// console.log(items["price"])
+
+// Operators
+
+// Types of operators
+
+// Arithmetic  operators  (+, -, *, /, %, **)
+// Assignment operators   (+=, -=, *=, /=, %=)
+// Comparison operators   (==, ===, !=, !==, >, <, >=, <=, )
+// Logical operators      (&&, ||, !)
+
+
+// Arithmetic operator
+
+
+
 
 
